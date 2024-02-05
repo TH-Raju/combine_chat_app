aa2fc706d8dd3a423788e80789b9ed410d229353
TH-Raju <rajukhan894200@gmail.com>
2024-02-06 04:46:10 +0600

    room create

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const io = new Server(server)
 io.on("connection", (socket)=> {
     console.log(socket.id, " User Connected.");
 
+    socket.join("kitchen");
+    let sizeOfRooms = io.sockets.adapter.rooms.get("kitchen").size;
+    io.sockets.in('kitchen').emit("cooking", "Fried Rice and chicken for " + sizeOfRooms )
+
+    socket.join("bed-room");
+    io.sockets.in('bed-room').emit("bedRoom", "Let's sleeping together")
+    io.sockets.in('bed-room').emit("restRoom", "I'm taking Rest")
+
 
     socket.on("chat", (data)=> {
         io.emit("chatShow", data)
